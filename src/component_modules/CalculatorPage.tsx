0382d468ff37ee2f4f924cecbf01db8e19fd225c
Raymond Zhuang <z5592481@ad.unsw.edu.au>
2026-05-19 17:09:47 +1000

    Created functionality to display elements on screen when corresponding button is pressed

--- src/component_modules/CalculatorPage.tsx
+++ src/component_modules/CalculatorPage.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 
@@ -5,6 +7,20 @@ import pageStyles from "../css_modules/PageStyles.module.css"
 import calculatorStyles from "../css_modules/CalculatorStyles.module.css"
 
 const CalculatorPage = () => {
+  const [expression, setExpression] = useState("");
+
+  const appendElement = (element: string) => {
+    setExpression(expression + element);
+  }
+
+  const backspace = () => {
+    setExpression(expression.slice(0, expression.length - 1));
+  }
+
+  const clearAll = () => {
+    setExpression("");
+  }
+  
   return (
     <>
       <Header />
@@ -16,27 +32,27 @@ const CalculatorPage = () => {
           </div>
           <i style={{fontSize: "75%"}}>NATURAL-V.P.A.M.</i>
           <div className={calculatorStyles["edition"]}>2nd edition</div>
-          <div className={calculatorStyles["expression-display"]}></div>
+          <div className={calculatorStyles["expression-display"]}>{expression}</div>
           <div className={calculatorStyles["basic-buttons"]}>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button className={calculatorStyles["deletion-button"]}>DEL</button>
-            <button className={calculatorStyles["deletion-button"]}>AC</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>*</button>
-            <button>/</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>+</button>
-            <button>-</button>
-            <button>0</button>
-            <button>.</button>
+            <button onClick={() => appendElement("7")}>7</button>
+            <button onClick={() => appendElement("8")}>8</button>
+            <button onClick={() => appendElement("9")}>9</button>
+            <button className={calculatorStyles["deletion-button"]} onClick={backspace}>DEL</button>
+            <button className={calculatorStyles["deletion-button"]} onClick={clearAll}>AC</button>
+            <button onClick={() => appendElement("4")}>4</button>
+            <button onClick={() => appendElement("5")}>5</button>
+            <button onClick={() => appendElement("6")}>6</button>
+            <button onClick={() => appendElement("*")}>*</button>
+            <button onClick={() => appendElement("/")}>/</button>
+            <button onClick={() => appendElement("1")}>1</button>
+            <button onClick={() => appendElement("2")}>2</button>
+            <button onClick={() => appendElement("3")}>3</button>
+            <button onClick={() => appendElement("+")}>+</button>
+            <button onClick={() => appendElement("-")}>-</button>
+            <button onClick={() => appendElement("0")}>0</button>
+            <button onClick={() => appendElement(".")}>.</button>
             <button></button>
-            <button>Ans</button>
+            <button onClick={() => appendElement("Ans")}>Ans</button>
             <button>=</button>
           </div>
         </div>

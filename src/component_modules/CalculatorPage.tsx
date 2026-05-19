b0a4e18293f21b0650373a48aa32528cd86fb361
Raymond Zhuang <z5592481@ad.unsw.edu.au>
2026-05-19 16:22:08 +1000

    Established general layout of basic buttons; inserted text such as brand, model, edition, etc.

--- src/component_modules/CalculatorPage.tsx
+++ src/component_modules/CalculatorPage.tsx
@@ -10,7 +10,35 @@ const CalculatorPage = () => {
       <Header />
       <div className={pageStyles["page"]}>
         <div className={calculatorStyles["calculator"]}>
+          <div className={calculatorStyles["brand-model"]}>
+            <b>CASIO</b>
+            <span style={{fontSize: "60%"}}>fx-82AU PLUS II</span>
+          </div>
+          <i style={{fontSize: "75%"}}>NATURAL-V.P.A.M.</i>
+          <div className={calculatorStyles["edition"]}>2nd edition</div>
           <div className={calculatorStyles["expression-display"]}></div>
+          <div className={calculatorStyles["basic-buttons"]}>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button className={calculatorStyles["deletion-button"]}>DEL</button>
+            <button className={calculatorStyles["deletion-button"]}>AC</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>*</button>
+            <button>/</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>-</button>
+            <button>0</button>
+            <button>.</button>
+            <button></button>
+            <button>Ans</button>
+            <button>=</button>
+          </div>
         </div>
       </div>
       <Footer />
